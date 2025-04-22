4122494c5c19b7fea4fcb6e6fcfc82f5c0c03c47
Heia Sarange Herrera <heiaherrera@gmail.com>
2025-04-22 18:53:31 +0800

    work with order table

--- src/main/java/com/herrera/models/Order.java
+++ src/main/java/com/herrera/models/Order.java
@@ -1,5 +1,17 @@
 package com.herrera.models;
 
+import java.util.List;
+
+import com.herrera.views.components.reusable.ProductContainer;
+
 public class Order {
     
+    List<ProductContainer> orderList;
+    private int order_number;
+
+
+    public void addToOrderList(ProductContainer product){
+        orderList.add(product);
+    }
+
 }
